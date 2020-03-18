5ede0d01a8b1ad127823524d65c790480c131d01
SHAILESH SHARMA <58656075+shaileshvns@users.noreply.github.com>
2020-03-18 18:15:24 +0530

    second commit

--- StockSpanProblem.cpp
+++ StockSpanProblem.cpp
@@ -3,38 +3,60 @@
 using namespace std; 
   
 
-void calculateSpan(int A[], int n, int ans[]) 
-{  
-    ans[0] = 1; 
-   
-    for (int i = 1; i < n; i++) { 
-        int counter = 1; 
-        while ((i - counter) >= 0 && A[i] > A[i - counter]) { 
-            counter += ans[i - counter]; 
-        } 
-        ans[i] = counter; 
-    } 
-} 
+#include<iostream>
+#include<stack>
+using namespace std;
+ class spanData{
+     
+     public:
+     int data;
+     int key = 1;
+
+ };
+void calculateSpan(spanData stockPrice[],int n){
+    stack<spanData> s1;
+    stack<spanData> s2;
+    s2.push(stockPrice[0]);
+    for (int i = 1; i < n; i++)
+    { 
+        spanData sp = s2.top();
+        if (!stockPrice[i].data < stockPrice[i-1].data)
+        {
+        
+       while(stockPrice[i].data > sp.data)
+       {   
+           s1.push(s2.top());
+           s2.pop();
+           if (s2.empty()){
+           break;}
+           sp = s2.top();
+           
+       }
+       s2.push(stockPrice[i]);
+       while (!s1.empty())
+       { 
+           stockPrice[i].key++;
+           s2.push(s1.top());
+           s1.pop();
+       }}
+  }
+
+}
+int main(){
+    int sp[] = { 100, 80, 60, 70, 60, 75, 85};   
+    int n = sizeof(sp)/sizeof(sp[0]);
+    spanData stockPrice[n];
+    for (int i = 0; i < n; i++)
+    stockPrice[i].data = sp[i];
+    
+    calculateSpan(stockPrice,n);
+    for(int i = 0; i < n; i++)
+    {
+        cout<<stockPrice[i].key<<" ";
+    }
+    
+    
+     return 0;
+ } 
   
 
-void printArray(int arr[], int n) 
-{ 
-    for (int i = 0; i < n; i++) 
-        cout << arr[i] << " "; 
-} 
-  
- 
-int main() 
-{ 
-    int price[] = { 10, 4, 5, 90, 120, 80 }; 
-    int n = sizeof(price) / sizeof(price[0]); 
-    int S[n]; 
-  
-     
-    calculateSpan(price, n, S); 
-  
-    
-    printArray(S, n); 
-  
-    return 0; 
-}
